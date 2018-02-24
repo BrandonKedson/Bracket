6f9ae965184fd14f5d0bd59a5cd053095da72221
Zac S <becausemountains@gmail.com>
2018-02-24 14:33:47 -0500

    V1

--- Bracket/ViewController.swift
+++ Bracket/ViewController.swift
@@ -10,9 +10,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playerValueDisplay: UILabel!
+    @IBOutlet weak var playerSliderValue: UISlider!
+    @IBOutlet weak var teamsOnDisplay: UILabel!
+    @IBOutlet weak var seedingOnDisplay: UILabel!
+    @IBOutlet weak var teamButton: UISwitch!
+    @IBOutlet weak var seedingButton: UISwitch!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    
+    @IBAction func nextClicked(_ sender: UIButton) {
+        performSegue(withIdentifier: "firstSegue", sender: self)
+    }
+    @IBAction func playerSlidingDone(_ sender: UISlider) {
+        if Int(playerSliderValue.value) > 1{
+            playerValueDisplay.text = "\(Int(playerSliderValue.value)) PLAYERS"
+        }
+        else{
+            playerValueDisplay.text = "\(Int(playerSliderValue.value)) PLAYER"
+        }
+        
+        if playerSliderValue.value < 4{
+            nextButton.isEnabled = false
+            UIView.animate(withDuration: 0.6, animations: {
+                    self.nextButton.alpha = 0.4
+                })
+        }
+        else{
+            nextButton.isEnabled = true
+            UIView.animate(withDuration: 0.6, animations: {
+                self.nextButton.alpha = 1
+            })
+        }
+        
+    }
+    @IBAction func teamButtonClicked(_ sender: UISwitch) {
+        if teamButton.isOn{
+            UIView.animate(withDuration: 0.6, animations: {
+                self.teamsOnDisplay.alpha = 1
+            })
+        }
+        else{
+            UIView.animate(withDuration: 0.6, animations: {
+                self.teamsOnDisplay.alpha = 0.4
+            })
+        }
+    }
+    @IBAction func seedingButtonClicked(_ sender: UISwitch) {
+        if seedingButton.isOn{
+            UIView.animate(withDuration: 0.6, animations: {
+                self.seedingOnDisplay.alpha = 1
+            })
+        }
+        else{
+            UIView.animate(withDuration: 0.6, animations: {
+                self.seedingOnDisplay.alpha = 0.4
+            })
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nextButton.isEnabled = false
+        UIView.animate(withDuration: 0.6, animations: {
+            self.nextButton.alpha = 0.4
+        })
     }
 
     override func didReceiveMemoryWarning() {
