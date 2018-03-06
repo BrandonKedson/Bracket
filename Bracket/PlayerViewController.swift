7baae579fda676325563c962898dd3cf4f7ee076
Brandon Kedson (student LM) <s012761@lm-19s012761.local>
2018-03-06 11:50:26 -0500

    First two views version one

--- Bracket/PlayerViewController.swift
+++ Bracket/PlayerViewController.swift
@@ -10,10 +10,81 @@ import UIKit
 
 class PlayerViewController: UIViewController {
 
+    var players = [String]()
+    @IBOutlet weak var verticalSlider: UISlider!{
+        didSet{
+            verticalSlider.transform = CGAffineTransform(rotationAngle: CGFloat.pi/2)
+        }
+    }
+    
+    @IBOutlet weak var playerText: UITextField!
+    
+    @IBAction func backButtonClicked(_ sender: UIButton) {
+        performSegue(withIdentifier: "backSegue", sender: self)
+    }
+    
+    @IBOutlet weak var firstPlayersDisplay: UILabel!
+    @IBOutlet weak var secondPlayersDisplay: UILabel!
+    @IBOutlet weak var thirdPlayersDisplay: UILabel!
+    @IBOutlet weak var fourthPlayersDisplay: UILabel!
+    
+    @IBAction func playerScroll(_ sender: UISlider) {
+        if players.count>Int(verticalSlider.value){
+            firstPlayersDisplay.text! = players[Int(verticalSlider.value)]
+        }
+        else{
+            firstPlayersDisplay.text! = ""
+        }
+        if players.count>Int(verticalSlider.value)+1{
+            secondPlayersDisplay.text! = players[Int(verticalSlider.value)+1]
+        }
+        else{
+            secondPlayersDisplay.text! = ""
+        }
+        if players.count>Int(verticalSlider.value)+2{
+            thirdPlayersDisplay.text! = players[Int(verticalSlider.value)+2]
+        }
+        else{
+           thirdPlayersDisplay.text! = ""
+        }
+        if players.count>Int(verticalSlider.value)+3{
+            fourthPlayersDisplay.text! = players[Int(verticalSlider.value)+3]
+        }
+        else{
+            fourthPlayersDisplay.text! = ""
+        }
+    }
+    @IBAction func addPlayer(_ sender: UIButton) {
+        players.append(playerText.text!)
+        print(players)
+        playerText.text = ""
+        firstPlayersDisplay.text?.removeAll()
+        secondPlayersDisplay.text?.removeAll()
+        thirdPlayersDisplay.text?.removeAll()
+        fourthPlayersDisplay.text?.removeAll()
+        
+        if players.count>Int(verticalSlider.value){
+        firstPlayersDisplay.text! = players[Int(verticalSlider.value)]
+        }
+        if players.count>Int(verticalSlider.value)+1{
+        secondPlayersDisplay.text! = players[Int(verticalSlider.value)+1]
+        }
+        if players.count>Int(verticalSlider.value)+2{
+        thirdPlayersDisplay.text! = players[Int(verticalSlider.value)+2]
+        }
+        if players.count>Int(verticalSlider.value)+3{
+        fourthPlayersDisplay.text! = players[Int(verticalSlider.value)+3]
+        }
+
+    }
+    
+    @IBAction func startButtonClicked(_ sender: UIButton) {
+         performSegue(withIdentifier: "secondSegue", sender: self)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        playerText.placeholder = "ENTER A NAME"
+        verticalSlider.setValue(0, animated: false)
     }
 
     override func didReceiveMemoryWarning() {
--- Bracket/ViewController.swift
+++ Bracket/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    let bracketTypes = ["SINGLE ELIMINATION", "ROUND ROBIN", "DOUBLE ELIMINATION"]
+    var selectedBracketType: String?
+    
     @IBOutlet weak var playerValueDisplay: UILabel!
     @IBOutlet weak var playerSliderValue: UISlider!
     @IBOutlet weak var teamsOnDisplay: UILabel!
@@ -17,7 +20,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var teamButton: UISwitch!
     @IBOutlet weak var seedingButton: UISwitch!
     @IBOutlet weak var nextButton: UIButton!
-    
+    @IBOutlet weak var blurEffect: UIVisualEffectView!
+    @IBOutlet weak var bracketTypeTextField: UITextField!
+    @IBOutlet weak var startButton: UIButton!
+
     
     @IBAction func nextClicked(_ sender: UIButton) {
         performSegue(withIdentifier: "firstSegue", sender: self)
@@ -68,13 +74,29 @@ class ViewController: UIViewController {
             })
         }
     }
+    @IBAction func clickedOffBracketType(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    func createBracketPicker() {
+        let bracketPicker = UIPickerView()
+        bracketPicker.delegate = self
+        bracketTypeTextField.inputView = bracketPicker
+        bracketPicker.backgroundColor = UIColor(displayP3Red: 255, green: 126, blue: 121, alpha: 1)
+    }
+    @IBAction func startButtonClicked(_ sender: UIButton) {
+        UIView.animate(withDuration: 1, animations: {
+            self.blurEffect.alpha = 0.7
+            self.startButton.alpha = 0
+        }, completion: { (complete: Bool) in
+            self.blurEffect.alpha = 0
+        })
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        createBracketPicker()
         nextButton.isEnabled = false
-        UIView.animate(withDuration: 0.6, animations: {
-            self.nextButton.alpha = 0.4
-        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -85,3 +107,20 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return bracketTypes.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return bracketTypes[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedBracketType = bracketTypes[row]
+        bracketTypeTextField.text = selectedBracketType
+    }
+    
+}
